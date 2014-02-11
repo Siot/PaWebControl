fc9081a863157a3bb67738fe1278907c6c085794
Siot <github@siot.es>
2014-02-11 11:16:36 +0100

    modified:   index.php 	new file:   jquery-2.1.0.min.js 	new file:   pactl.css

--- source/index.php
+++ source/index.php
@@ -3,31 +3,9 @@
 <head>
 	<meta charset=utf-8 />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	<script type="text/javascript" src="http://code.jquery.com/jquery-2.1.0.min.js"></script>
+	<script type="text/javascript" src="jquery-2.1.0.min.js"></script>
 	<script type="text/javascript" src="pactl.js"></script>
-	<style>
-		input[type="range"]{
-			height: 40px;
-			width: 99%;
-			margin: 0;
-			vertical-align:middle;
-		}
-		input[type="text"]{
-			width: 3em;
-			text-align:center;
-			vertical-align:middle;
-		}
-		.inputsink{
-			background-color: #d3d9c6;
-		}
-		#sinks div{
-			padding:2px 0;
-		}
-		body{
-			margin:0;
-			padding:0;
-		}
-	</style>
+	<link href="pactl.css" rel="stylesheet">
 </head>
 <body>
 	<div id="sinks"></div>
